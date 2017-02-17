6e2dcaf314738e6180850b18c0f6706e249f2f79
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-02-17 18:04:16 +0500

    Improve clarity in the toggleComplete function

--- src/Todo/TodoHelper.js
+++ src/Todo/TodoHelper.js
@@ -2,7 +2,6 @@ export const addTask = (task, tasks) => [...tasks, task];
 
 export const toggleComplete = (taskTitle, tasks) => {
   const taskIndex = tasks.findIndex(task => task.title === taskTitle);
-  const task = Object.assign({}, tasks[taskIndex]);
-  task.complete = !task.complete;
+  const task = Object.assign({}, tasks[taskIndex], { complete: !tasks[taskIndex].complete });
   return [...tasks.slice(0, taskIndex), task, ...tasks.slice(taskIndex + 1)];
 };

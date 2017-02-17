3a2c455ea0aa9dc8f39ca15b94ce20c2bd9e9afb
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-02-17 17:51:52 +0500

    Added the deep freeze library for the tests of immuttable

--- src/Todo/TodoHelper.test.js
+++ src/Todo/TodoHelper.test.js
@@ -1,3 +1,4 @@
+import deepFreeze from 'deep-freeze';
 import { addTask, toggleComplete } from './TodoHelper';
 
 describe('addTask', () => {
@@ -5,16 +6,9 @@ describe('addTask', () => {
     const tasks = [{ title: 'first', complete: false }];
     const newTask = { title: 'second', complete: false };
     const expected = [{ title: 'first', complete: false }, { title: 'second', complete: false }];
+    deepFreeze(tasks);
     const result = addTask(newTask, tasks);
     expect(result).toEqual(expected);
-  });
-
-  it('addTask should not mutate the original tasks list', () => {
-    const tasks = [{ title: 'first', complete: false }];
-    const newTask = { title: 'second', complete: false };
-    const expected = [{ title: 'first', complete: false }];
-    addTask(newTask, tasks);
-    expect(tasks).toEqual(expected);
   });
 });
 
@@ -22,18 +16,13 @@ describe('toggleComplete', () => {
   it('toggleComplete must invert complete status of the task', () => {
     const tasks = [{ title: 'first', complete: false }];
     const expected = [{ title: 'first', complete: true }];
+    deepFreeze(tasks);
     const result = toggleComplete('first', tasks);
     expect(result).toEqual(expected);
 
     const expected2 = [{ title: 'first', complete: false }];
+    deepFreeze(result);
     const result2 = toggleComplete('first', result);
     expect(result2).toEqual(expected2);
-  });
-
-  it('toggleComplete should not mutate the original tasks list', () => {
-    const tasks = [{ title: 'first', complete: false }];
-    const expected = [{ title: 'first', complete: false }];
-    toggleComplete('first', tasks);
-    expect(tasks).toEqual(expected);
   });
 });

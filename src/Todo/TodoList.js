e08e3f65b8a8a47c84569364b56adbe78d269d4e
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-02-17 22:21:58 +0500

    Converted TodoList to pure function

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-const TodoList = ({ tasks, onToggleActive }) => {
-  const items = tasks.map(task =>
-    <TodoItem key={task.title} {...task} onToggleActive={onToggleActive} />);
-  return (
-    <ul>
-      { items }
-    </ul>);
-};
+const TodoList = ({ tasks, onToggleActive }) =>
+  <ul>
+    {tasks.map(task =>
+      <TodoItem
+        key={task.title}
+        {...task}
+        onToggleActive={onToggleActive}
+      />)
+    }
+  </ul>;
 
 TodoList.propTypes = {
   tasks: React.PropTypes.arrayOf(React.PropTypes.shape({ title: '', complete: false })).isRequired,

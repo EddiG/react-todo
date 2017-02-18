e6e7f890f0bd15a356d0d2694dccded7243b55f7
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-02-18 21:18:14 +0500

    Added redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
+const store = createStore();
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );

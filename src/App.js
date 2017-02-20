a152c0875f43cf5bc70eb08a7375d57eca709e38
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-02-20 17:01:59 +0500

    Cleared up the project

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import todoApp from './reducers/';
-import logo from './logo.svg';
 import Todo from './components/Todo';
-import './App.css';
 
 const store = createStore(todoApp);
 
 const App = () =>
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Hello world!</h2>
-    </div>
+  <div>
     <Todo />
   </div>;
 

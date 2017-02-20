d8830f9986931558fdec3a0bd2c05b75ae470b22
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-02-20 19:36:45 +0500

    Added a redux logger

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import todoApp from './reducers/';
 import Todo from './components/Todo';
 
-const store = createStore(todoApp);
+const store = createStore(todoApp, applyMiddleware(createLogger()));
 
 // Needed for onTouchTap for MUI library
 // http://stackoverflow.com/a/34015469/988941
